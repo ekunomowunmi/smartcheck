c225287553791e75a099df2b85a1071d83ed90b3
Omowunmi <omowunmi.ekun@xputertechnologies.com>
2021-06-11 01:02:13 +0100

    stickytop;routerfix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'service', loadChildren: () => import('./service/service.module').then(m => m.ServiceModule) },
-  { path: 'application', loadChildren: () => import('./application/application.module').then(m => m.ApplicationModule) }];
+  { path: 'application', loadChildren: () => import('./application/application.module').then(m => m.ApplicationModule) },
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
